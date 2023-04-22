8c69e9e82c3a55281df9020f9b57c53c838ec141
Atul Nag <130294960+atulnag2@users.noreply.github.com>
2023-04-22 11:30:32 +0530

    Update extractFasta.py
    The purpose of this program is to extract required fasta sequence of the required data.

--- extractFasta.py
+++ extractFasta.py
@@ -2,7 +2,7 @@ import sys
 import re
 import subprocess
 
-
+# Usage
 if len(sys.argv) < 3:
 	print("usage: python3 extractFasta.py <id list> <fasta db> <output>")
 	sys.exit(1)
@@ -18,11 +18,12 @@ seq = ''
 
 idlist = []
 
+# Read ID File
 with open(idFile) as IDS:
 	for line in IDS:
 		line = line.strip()
 		idlist.append(line)
-
+# Read FASTA file
 with open(fastaFile) as FASTA:
 	for line in FASTA:
 		if line.startswith(">"):
@@ -36,7 +37,7 @@ with open(fastaFile) as FASTA:
 			seq = seq + line
 	fdb[header] = seq
 
-
+# Write to Output File
 with open(outFile,"w") as OUT:
 	for id in idlist:
 		for key in fdb.keys():
